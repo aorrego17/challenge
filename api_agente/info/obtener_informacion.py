6fc5edc8478b6b8fe96272fe91f2c7423dc18ad3
aorrego17 <aorrego17@gmail.com>
2022-08-24 01:36:59 -0500

    Modificación archivo obtener_informacion.py

--- api_agente/info/obtener_informacion.py
+++ api_agente/info/obtener_informacion.py
@@ -1,7 +1,6 @@
-import array
 import psutil
 import platform
-
+## def significa funcion
 def Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador):
     array={}
     array['version']=VersionOS
@@ -13,7 +12,6 @@ def Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador):
 def Obtener_info_sistema_operativo():
     NameOS = platform.system()
     if NameOS == "Darwin":
-        print(NameOS)
         VersionOS = platform.mac_ver()[0]
         UsuariosActivos = psutil.users()[0].name
         InfoProcesador = platform.uname()[3]
@@ -21,28 +19,25 @@ def Obtener_info_sistema_operativo():
         MaquinaPS = psutil.pids()
         for procesos in MaquinaPS:
             DetalleProceso = psutil.Process(procesos)
-        return  Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
+        return Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
     elif NameOS == "Linux":
-        print(NameOS)
         VersionOS = platform.libc_ver()[0]
         UsuariosActivos = psutil.users()
         InfoProcesador = platform.uname()[3]
         Procesador = platform.processor()
-        Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
         MaquinaPS = psutil.pids()
         for procesos in MaquinaPS:
             DetalleProceso = psutil.Process(procesos)
             print(DetalleProceso)  
+        return Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
     else:
-        print(NameOS)
         VersionOS = platform.win32_ver()[0]
         UsuariosActivos = psutil.users()
         InfoProcesador = platform.uname()[3]
         Procesador = platform.processor()
-        Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
         MaquinaPS = psutil.pids()
         for procesos in MaquinaPS:
             DetalleProceso = psutil.Process(procesos)
             print(DetalleProceso)
-    return NameOS
+        return Imprimir_resultados(VersionOS,UsuariosActivos,InfoProcesador,Procesador)
         
